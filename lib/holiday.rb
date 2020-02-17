7db0323d03f758217fdec1d99a722ec064d9b527
norio c <norionorioc@gmail.com>
2020-02-17 01:39:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,6 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
   
     holiday_hash.each do |seasons, holidays| 
       puts "#{season.capitalize}:"
+      holiday.each do 
 end
 
 def all_holidays_with_bbq(holiday_hash)
