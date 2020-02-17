749de15392dbafad59c948bb8eed06bfbc5ebb6f
norio c <norionorioc@gmail.com>
2020-02-17 01:46:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.collect |
+  holiday_hash.collect |season, holidays|
+  
 end
 
 
