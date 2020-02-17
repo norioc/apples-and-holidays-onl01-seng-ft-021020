e5c77af42f2828ab150a0148f0312a22c03e0eb5
norio c <norionorioc@gmail.com>
2020-02-17 01:47:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,8 +77,10 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.collect do |season, holidays|
   holidays.collect do |name, activities|
-    name if activities.include? ("")
+    name if activities.include? ("BBQ")
 end
+holiday_hash.flatten
+
 
 
 
